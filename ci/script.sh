b383716655d9177a6387142c00544c7bc4a509e3
James Kominick <james.kominick@gmail.com>
2017-06-16 00:20:38 -0400

    fix travis tests

--- ci/script.sh
+++ ci/script.sh
@@ -13,7 +13,8 @@ main() {
 
     #cross test --target $TARGET
     cross test --target $TARGET --release
-    cd migrant_lib && cross test --target $TARGET --release
+    cd migrant_lib && cargo test
+    #cross test --target $TARGET --release
 
     #cross run --target $TARGET
     #cross run --target $TARGET --release
