5a1b87aff8cddc63b6dfaef73cac5ba7bd1f7be3
James Kominick <james.kominick@gmail.com>
2017-06-28 21:56:11 -0400

    update ci builds
    - build releases with all `features`

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin migrant --target $TARGET --release -- -C lto
+    cross rustc --bin migrant --target $TARGET --release --features 'postgresql sqlite' -- -C lto
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/migrant $stage/
