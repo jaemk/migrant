040c628b860b213c0119a295524ba3a20028374c
James Kominick <james.kominick@gmail.com>
2017-07-03 22:02:19 -0400

    update ci
    - test with features

--- ci/script.sh
+++ ci/script.sh
@@ -13,11 +13,8 @@ main() {
 
     #cross test --target $TARGET
     cross test --target $TARGET --release
-    cd migrant_lib && cargo test
-    #cross test --target $TARGET --release
-
-    #cross run --target $TARGET
-    #cross run --target $TARGET --release
+    cross test --target $TARGET --release --features postgresql
+    cross test --target $TARGET --release --features sqlite
 }
 
 # we don't run the "test phase" when doing deploys
