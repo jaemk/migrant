a8d530c776f160b14bab203ec3ce726e64b45f17
James Kominick <james.kominick@gmail.com>
2017-11-26 23:12:54 -0500

    update docs

--- migrant_lib/src/types.rs
+++ migrant_lib/src/types.rs
@@ -1,3 +1,10 @@
+/*!
+Re-exported database-specific drivers
+
+When built with database-specific features, this module will contain
+re-exported connection types (`rusqlite` / `postgres`)
+
+*/
 
 #[cfg(feature="postgresql")]
 pub use postgres::*;
