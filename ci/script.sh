4189eb62b01ee80c51711464d6a8e83d43cefe2b
James Kominick <james@kominick.com>
2018-02-27 00:13:57 -0500

    fix ci integration tests
    - run integration tests outside of docker containers (without `cross`)
    - run binary with features

--- ci/script.sh
+++ ci/script.sh
@@ -14,7 +14,9 @@ main() {
 
     #cross test --target $TARGET
     #cross test --target $TARGET --release
-    cross test --target $TARGET --release --features 'sqlite postgres mysql update'
+    #cross test --target $TARGET --release --features 'sqlite postgres mysql update'
+    cargo test
+    cross build --target $TARGET --release --features 'sqlite postgres mysql update'
 }
 
 # we don't run the "test phase" when doing deploys
--- tests/migrant.rs
+++ tests/migrant.rs
@@ -6,27 +6,27 @@ use assert_cli::Assert;
 #[test]
 fn kitchen_sink() {
     // make sure we're setup and back to no applied migrations
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["setup"])
         .unwrap();
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["apply", "-ad"])
         .execute().is_ok();
 
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["apply", "-ad"])
         .fails()
         .stderr().contains("MigrationComplete: No un-applied `Down` migrations found")
         .unwrap();
 
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["list"])
         .stdout().contains("Current Migration Status:")
         .stdout().contains("[ ] 20170812145327_initial")
         .stdout().contains("[ ] 20171126194042_second")
         .unwrap();
 
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["apply", "-a"])
         .stdout().contains("Applying[Up]:")
         .stdout().contains("Current Migration Status:")
@@ -34,14 +34,14 @@ fn kitchen_sink() {
         .stdout().contains("[✓] 20171126194042_second")
         .unwrap();
 
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["list"])
         .stdout().contains("Current Migration Status:")
         .stdout().contains("[✓] 20170812145327_initial")
         .stdout().contains("[✓] 20171126194042_second")
         .unwrap();
 
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["redo"])
         .stdout().contains("Applying[Down]:")
         .stdout().contains("Current Migration Status:")
@@ -51,7 +51,7 @@ fn kitchen_sink() {
         .stdout().contains("[✓] 20171126194042_second")
         .unwrap();
 
-    Assert::main_binary()
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
         .with_args(&["redo", "--all"])
         .stdout().contains("Applying[Down]:")
         .stdout().contains("Current Migration Status:")
@@ -62,5 +62,9 @@ fn kitchen_sink() {
         .stdout().contains("[✓] 20170812145327_initial")
         .stdout().contains("[✓] 20171126194042_second")
         .unwrap();
+
+    Assert::command(&["cargo", "run", "--features", "sqlite", "--"])
+        .with_args(&["apply", "-ad"])
+        .execute().is_ok();
 }
 
